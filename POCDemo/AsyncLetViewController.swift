4b9b1efeead866647b9daa7be2dbd7cafef980c0
DevendranathReddi <devendranath.reddy@techholding.co>
2023-02-14 15:11:07 +0530

    Initial Commit

---
+++ POCDemo/AsyncLetViewController.swift
@@ -0,0 +1,99 @@
+//
+//  AsyncLetViewController.swift
+//  POCDemo
+//
+//  Created by DNREDDi on 27/01/23.
+//
+
+import UIKit
+
+class AsyncLetViewController: UIViewController {
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        imageView1.setBorder()
+        imageView2.setBorder()
+        imageView3.setBorder()
+        imageView4.setBorder()
+    }
+    
+    @IBAction func performWithAsyncLet() {
+        // Resetting the image with placeholder
+        imageView1.reset()
+        imageView2.reset()
+        imageView3.reset()
+        imageView4.reset()
+        
+        Task {
+            async let image1 = HTTPClient.shared.loadImage(number: 1)
+            async let image2 = HTTPClient.shared.loadImage(number: 2)
+            async let image3 = HTTPClient.shared.loadImage(number: 3)
+            async let image4 = HTTPClient.shared.loadImage(number: 4)
+            
+            let images =  [try await image1, try await image2, try await image3, try await image4]
+            
+            imageView1.image = images[0]
+            imageView2.image = images[1]
+            imageView3.image = images[2]
+            imageView4.image = images[3]
+        }
+    }
+    
+    @IBAction func performWithTaskGroup() { // Wrap dependent tasks
+        imageView1.reset()
+        imageView2.reset()
+        imageView3.reset()
+        imageView4.reset()
+        
+        let vm = ViewModel()
+        Task {
+            let images = try await vm.fetchImages()
+            imageView1.image = images[0]
+            imageView2.image = images[1]
+            imageView3.image = images[2]
+            imageView4.image = images[3]
+        }
+    }
+}
+
+class ViewModel {
+    
+    func fetchImages() async throws -> [UIImage] {
+        
+        return try await withThrowingTaskGroup(of: UIImage.self) { group -> [UIImage] in
+            group.addTask(operation: {
+                try await HTTPClient.shared.loadImage(number: 1)
+            })
+            
+            group.addTask(operation: {
+                try await HTTPClient.shared.loadImage(number: 2)
+            })
+            
+            group.addTask(operation: {
+                try await HTTPClient.shared.loadImage(number: 3)
+            })
+            
+            group.addTask(operation: {
+                try await HTTPClient.shared.loadImage(number: 4)
+            })
+            
+            var images: [UIImage] = []
+            
+            for try await image in group {
+                images.append(image)
+            }
+            return images
+        }
+    }
+}
